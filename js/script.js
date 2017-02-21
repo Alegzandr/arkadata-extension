87d554859201e64a09e6039e0a60582d1506a5cc
Alexandre Farrenq <alexandrefarrenq@gmail.com>
2017-02-21 08:44:43 +0100

    Update popup

--- js/script.js
+++ js/script.js
@@ -7,14 +7,24 @@ $(function () {
   var apiStreams = 'https://api.twitch.tv/kraken/streams/' + channel + clientId;
   var apiChannels = 'https://api.twitch.tv/kraken/channels/' + channel + clientId;
 
-  // Load status
-  $.get(apiStreams, function (data) {
-    if (data.stream !== null)
-      $('.status').html('Stream online')
-  });
-
   // Load title
   $.get(apiChannels, function (data) {
-    $('.title').html(data.status);
+    $('main').html('<h3>' + data.status + '</h3>');
+    $('footer').animate({left: '0'}, 'slow', 'swing');
+    $('main > h3').fadeIn('slow');
+
+    // Load status
+    $.get(apiStreams, function (data) {
+      if (data.stream === null) {
+        $('main').css('border', 'inset 1px #ff0000').append('<small class="offline">Stream offline</small>');
+        $('footer').css('background', '#c00a33');
+      }
+      else {
+        $('main').css('border', 'inset 1px #2fd291').append('<small class="online">Stream online</small>');
+        $('footer').css('background', '#2fd291');
+      }
+
+      $('main > small').fadeIn('slow');
+    });
   });
 });
